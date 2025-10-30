3363792f16cb506f3230e8ac63b3287c01846258
Murilo Souza <murilos@unicamp.br>
2025-10-30 13:45:25 -0300

    docs: atualizar instruções de instalação

--- templates/_layout_start.php
+++ templates/_layout_start.php
@@ -7,18 +7,20 @@
     <?php echo get_component('default', 'tagManager', ['code' => 'script']); ?>
     <link rel="shortcut icon" href="<?php echo public_path('favicon.ico'); ?>">
  
-    <!-- CSS Bootstraṕ -->
+    <!-- import bootstrap icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 
-    <script defer src="/dist/js/vendor.bundle.1697f26639ee588df9ee.js"></script>
-    <script defer src="/dist/js/arDominionB5Plugin.bundle.e0fc15ed92535143cfc2.js"></script>
-    <link href="/dist/css/arArchivesRedisapPlugin.bundle.754d1b52f116f8298b36.css" rel="stylesheet">
+    <!-- important: hash files can change. Update! -->
+    <script defer src="/dist/js/vendor.bundle.51cf3c3989802bda6211.js"></script>
+    <script defer src="/dist/js/arDominionB5Plugin.bundle.ee01f345bf45cf8bd842.js"></script>
+    <link href="/dist/css/arDominionB5Plugin.bundle.5e47f9f2330e33f54f04.css" rel="stylesheet">
 
     <?php echo get_component_slot('css'); ?>
 
+    <!-- customizations -->
     <script defer src="/plugins/arArchivesRedisapPlugin/custom-scripts.js"></script>
     <link href="/plugins/arArchivesRedisapPlugin/custom-styles.css" rel="stylesheet" >
 
